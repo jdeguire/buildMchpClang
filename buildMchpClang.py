a0e5e52a02e0ac4f1c160eb4c633ef5f5d6489d5
Jesse DeGuire <jesse.a.deguire@gmail.com>
2026-09-15 10:56:43 -0500

    Update versions

--- buildMchpClang.py
+++ buildMchpClang.py
@@ -57,7 +57,7 @@ import tarfile
 import time
 import zipfile
 
-MCHP_CLANG_VERSION = '0.8.3'
+MCHP_CLANG_VERSION = '0.9.0'
 MCHP_CLANG_PROJECT_URL = 'https://github.com/jdeguire/buildMchpClang'
 
 
@@ -74,7 +74,7 @@ CMAKE_CACHE_DIR = THIS_FILE_DIR / 'cmake_caches'
 TESTS_DIR = THIS_FILE_DIR / 'test'
 
 LLVM_REPO_URL = 'https://github.com/llvm/llvm-project.git'
-LLVM_REPO_BRANCH = 'llvmorg-23.1.0'
+LLVM_REPO_BRANCH = 'llvmorg-23.1.1'
 LLVM_SRC_DIR = ROOT_WORKING_DIR / 'llvm'
 
 CMSIS_REPO_URL = 'https://github.com/ARM-software/CMSIS_6.git'
@@ -82,7 +82,7 @@ CMSIS_REPO_BRANCH = 'v6.3.0'
 CMSIS_SRC_DIR = ROOT_WORKING_DIR / 'cmsis'
 
 ATDF_FILE_MAKER_REPO_URL = 'https://github.com/jdeguire/atdf-device-file-maker.git'
-ATDF_FILE_MAKER_REPO_BRANCH = 'v0.7.4'
+ATDF_FILE_MAKER_REPO_BRANCH = 'v0.8.0'
 ATDF_FILE_MAKER_SRC_DIR = ROOT_WORKING_DIR / 'atdf-device-file-maker'
 
 MCHPCLANG_DOCS_REPO_URL = 'https://github.com/jdeguire/mchpclang_docs.git'
